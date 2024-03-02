f800e5ffeb235982cc0209810c84e1f8e92fb2bd
Kezzy Ngotho <keziengotho18@gmail.com>
2024-03-02 14:00:54 +0300

    initial

---
+++ inventory-api/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const db = require("./queries"); // Import database queries
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+app.get("/", (request, response) => {
+  response.json({ info: "Node.js, Express, and Postgres API" });
+});
+
+// Define routes using imported database query functions
+app.get("/inventory", getAllInventory);
+app.get("/inventory/:id", getInventoryById);
+app.post("/inventory", createInventoryItem);
+app.put("/inventory/:id", updateInventoryItem);
+app.delete("/inventory/:id", deleteInventoryItem);
+
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}.`);
+});
